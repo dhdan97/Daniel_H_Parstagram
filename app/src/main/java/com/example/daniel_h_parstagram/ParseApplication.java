8ab7ae7729ea69f511a3811bdb6124b1881943cc
Daniel Hernandez <dhdan97@gmail.com>
2021-03-11 14:46:32 -0500

    Added user sign in, user take photo and upload

--- app/src/main/java/com/example/daniel_h_parstagram/ParseApplication.java
+++ app/src/main/java/com/example/daniel_h_parstagram/ParseApplication.java
@@ -3,12 +3,16 @@ package com.example.daniel_h_parstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("kQsilkum6JOztF8dvedahquN5UcZ63tk3pLpYM5Q")
